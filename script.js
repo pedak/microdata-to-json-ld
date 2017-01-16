6e5038d432e60a2ea2b5f22e60c788a3885bb8f7
pedak <peter.kalchgruber@univie.ac.at>
2017-01-16 18:23:08 +0100

    minor bugfixes

--- script.js
+++ script.js
@@ -1,70 +1,111 @@
-function cleaning(val){
-  if(typeof val == "undefined" || typeof val === "object")
-    return val;
-  return (val.replace(/(\r\n|\n|\r})/gm,"").replace(/\s\s+/g," ")).trim();
-}
-
-function append(el,key,val){
-  val = cleaning(val);
-  if(key in el && Array.isArray(el[key])){
-    el[key].push(val);
-  }else if(key in el){
-    el[key] = jQuery.makeArray(el[key]);
-    el[key].push(val);
-  }else{
-    el[key] = val;
-  }
-  return el;
-}
-
-/*
-convert all microdata elements in json-ld format.
-Need to filter to only convert top-itemscope elements and further to only process 
-itemprops that are direct child of current itemscope element and not process values
-that contain other itemprop elements
+/**
+* A JavaScript implementation of Microdata to JSON-LD Converter
+* @author Peter Kalchgruber
+*
+* Copyright 2017,  All rights reserved.
 */
-function convert(){
-  var type = [];
-  var item = [];
-  $('[itemscope]').map(function(i, el){
-    var im = {};
-    if($(el).parents("[itemscope]").length == 0){ 
-      type.push($(this).attr("itemtype"));
-      $(el).find("[itemprop]").attr("itemprop", function(i, key) {
-        if ($(this).parents("[itemscope]").get(0) == el && $(this).has("[itemprop]").length == 0){ 
-          val = $(this).attr("href") || $(this).prop("content") || $(this).text() || $(this).attr("src");
-          im = append(im,key,val);
-          im["@type"] = $(el).attr("itemtype");
-        }
-        if ($(this).is("[itemscope]") && $(this).is("[itemprop]")) {
-          var subitem = {};
-          subitem["@type"] = $(this).attr("itemtype");
-          type.push($(this).attr("itemtype"));
-          $(this).find("[itemprop]").attr("itemprop", function(i, subkey) {
-            subval = $(this).attr("href") || $(this).prop("content") || $(this).text() || $(this).attr("src");
-            subitem = append(subitem,subkey,subval);
-          });
-          im  =  append(im,$(this).attr("itemprop"),subitem);
+
+
+(function(window){
+  'use strict';
+  function convert(){
+    var _converterObject = {};
+
+    /**
+     * Remove Spaces, Linebreaks...
+     * @param {String} val
+     * @return {String} val
+     */
+    function _clean(val){
+      if(typeof val == 'undefined' || typeof val === 'object')
+        return val;
+      return (val.replace(/(\r\n|\n|\r})/gm,'').replace(/\s\s+/g,' ')).trim();
+    }
+
+    /**
+     * Retrieve values of HTML element 
+     * @param {DOM object} el
+     * @return {String} value
+     */
+    function _getval(el){
+      return _clean($(el).attr('href') || $(el).prop('content') || $(el).text() || $(el).attr('src'));
+    }
+
+    /**
+     * Append value to json-ld object. If property is already assigned, create array and return object
+     * @param {object} el
+     * @param {String} key
+     * @param {String} val
+     * @return {object} el
+     */
+    function _append(el,key,val){
+      if(key in el && Array.isArray(el[key])){
+        el[key].push(val);
+      }else if(key in el){
+        el[key] = jQuery.makeArray(el[key]);
+        el[key].push(val);
+      }else{
+        el[key] = val;
+      }
+      return el;
+    }
+
+    /**
+     * Convert Microdata element to JSON-LD object
+     * @param {object} topel 
+     * @return {object} im
+     */
+    function _get(topelem){
+      var im={};
+      im['@type'] = $(topelem).attr('itemtype');
+      $(topelem).find('[itemprop]').attr('itemprop', function(i, key) {
+        if ($(this).parents('[itemscope]').get(0) == topelem){
+          if ($(this).is('[itemscope]') && $(this).is('[itemprop]')) {
+            var subitem = _get(this);
+            im = _append(im,$(this).attr('itemprop'),subitem);
+          }else{
+            var val = _getval(this);
+            im = _append(im,key,val);
+          }
         }
       });
-      item.push(im);
+      return im;
     }
-  });
-  console.log(type);
-  if(item.length>1){
-    jsonld={}
-    jsonld["@context"] = "http://schema.org/";
-    jsonld['@graph'] = item
-    return jsonld
-  }else{
-    item[0]['@context'] = "http://schema.org/";
-    return item[0];
+
+    /**
+    *  Convert all microdata elements in json-ld format.
+    *  Need to filter to only convert top-itemscope elements and further to only process 
+    *  itemprops that are direct child of current itemscope element and not process values
+    *  that contain other itemprop elements
+    *  @return {Object} item
+    */
+    _converterObject.convert = function(){
+      var item = [];
+      $('[itemscope]').map(function(i, el){
+        if($(el).parents('[itemscope]').length == 0){ //only proceed with top itemscope entries
+          item.push(_get(el));
+        }
+      });
+      
+      if(item.length>1){
+        jsonld={};
+        jsonld['@context'] = 'http://schema.org/';
+        jsonld['@graph'] = item
+        return jsonld;
+      }else{
+        item['@context'] = 'http://schema.org/';
+        return item;
+      }
+
+    }
+    return _converterObject;
+
   }
 
-}
 
-output = convert();
-console.log(JSON.stringify(output, null, 2));
-$("body").prepend(
-  "<pre>" + JSON.stringify(output, null, 2) + "<pre>"
-);
+
+  if(typeof(window.converter) === 'undefined'){
+    window.converter = convert();
+  }
+})(window); // We send the window variable withing our function
+
